ef3c723f31039e9f28675a0c5d5fddd33a1e4370
Policepenguin13 <talia.bruckard@gmail.com>
2025-05-30 10:22:24 +1000

    Flower basics
    Made flowers function properly, but they don't change things yet.
    
    To do:
    - NPCs wandering
    - Mushroom startle
    - Quests
    - UI

--- Assets/SCRIPTS/Flower.cs
+++ Assets/SCRIPTS/Flower.cs
@@ -10,15 +10,56 @@ public class Flower : MonoBehaviour
 
     public GameObject[] flowerPatches;
 
+    public int choosing;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        RunAway();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        Debug.Log(this.gameObject + "Collided with " + collision.gameObject);
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log(this.gameObject + "Collided with THE PLAYER!1!");
+            RunAway();
+        }
+        else if (collision.gameObject.CompareTag("MUSHROAM"))
+        {
+            Debug.Log(this.gameObject + "Collided with a Mushroom!!!!");
+            RunAway();
+        }
+        else if (collision.gameObject.CompareTag("FLOWER"))
+        {
+            Debug.Log(this.gameObject + "Collided with a flower...");
+            ShuffleAway();
+        }
+        else if (collision.gameObject.CompareTag("ENVIRON"))
+        {
+            Debug.Log(this.gameObject + "collided with environment");
+            ShuffleAway();
+        }
+    }
+
+    public void ShuffleAway()
+    {
+        Debug.Log("Shuffling Away!!!!!");
+        transform.Translate(Random.Range(10, 50), 0, Random.Range(10, 50));
+        // float Xdist = flowerPatches[choosing].transform.position.x - transform.position.x;
+        // float Zdist = flowerPatches[choosing].transform.position.z - transform.position.z;
+        // Debug.Log("changed position by " + Xdist.ToString() + ", 0, " + Zdist.ToString());
+    }
+
+    public void RunAway()
+    {
+        choosing = Random.Range(0, flowerPatches.Length);
+        transform.position = flowerPatches[choosing].transform.position;
+        Debug.Log(this.gameObject + "choosing = " + choosing.ToString());
+        transform.Translate(Random.Range(10,50), 0, Random.Range(10, 50));
+        // float Xdist = flowerPatches[choosing].transform.position.x - transform.position.x;
+        // float Zdist = flowerPatches[choosing].transform.position.z - transform.position.z;
+        // Debug.Log(this.gameObject + "changed position by " + Xdist.ToString() + ", 0, " + Zdist.ToString());
     }
 }
